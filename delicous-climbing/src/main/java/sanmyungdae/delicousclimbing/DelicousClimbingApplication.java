003a7fc6d4469c44a1c407def598160d5156480d
Kim changwook <ckddnr0408@gmail.com>
2023-03-12 14:27:22 +0900

    create project

---
+++ delicous-climbing/src/main/java/sanmyungdae/delicousclimbing/DelicousClimbingApplication.java
@@ -0,0 +1,13 @@
+package sanmyungdae.delicousclimbing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelicousClimbingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelicousClimbingApplication.class, args);
+	}
+
+}
---
+++ delicous-climbing/src/test/java/sanmyungdae/delicousclimbing/DelicousClimbingApplicationTests.java
@@ -0,0 +1,13 @@
+package sanmyungdae.delicousclimbing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelicousClimbingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d6576223f39057952f683f823ab3e7f169da9e75
pmsu2007 <pmsu2008@gmail.com>
2023-05-25 02:05:08 +0900

    Refactor: 유저 서비스 & DTO & Entity 수정

--- delicious-climbing/src/main/java/sangmyungdae/deliciousclimbing/domain/entity/TbUser.java
+++ delicious-climbing/src/main/java/sangmyungdae/deliciousclimbing/domain/entity/TbUser.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import sangmyungdae.deliciousclimbing.domain.enums.Difficulty;
 import sangmyungdae.deliciousclimbing.domain.enums.Gender;
 import sangmyungdae.deliciousclimbing.domain.enums.LoginType;
+import sangmyungdae.deliciousclimbing.domain.enums.Role;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -22,15 +23,19 @@ public class TbUser extends TbDateEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private LoginType type;
-
     @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private LoginType type;
 
     @Column(nullable = false)
     private String nickname;
@@ -73,12 +78,14 @@ public class TbUser extends TbDateEntity {
     private List<TbEquipmentReview> equipmentReviews = new ArrayList<>();
 
     @Builder
-    public TbUser(LoginType type, String email, String password, String nickname, String imageUrl, String introduction,
+    public TbUser(String email, String password, Role role, LoginType type, String nickname, String imageUrl, String introduction,
                   Difficulty difficulty, String sns, Gender gender, LocalDateTime birthday,
                   TbFamousMountain famousMountain, TbAddress address) {
-        this.type = type;
+
         this.email = email;
         this.password = password;
+        this.role = role;
+        this.type = type;
         this.nickname = nickname;
         this.imageUrl = imageUrl;
         this.introduction = introduction;
--- delicious-climbing/src/main/java/sangmyungdae/deliciousclimbing/dto/user/UserRegister.java
+++ delicious-climbing/src/main/java/sangmyungdae/deliciousclimbing/dto/user/UserRegister.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import sangmyungdae.deliciousclimbing.domain.entity.TbUser;
 import sangmyungdae.deliciousclimbing.domain.enums.Gender;
 import sangmyungdae.deliciousclimbing.domain.enums.LoginType;
+import sangmyungdae.deliciousclimbing.domain.enums.Role;
 
 import java.time.LocalDateTime;
 
@@ -14,13 +15,14 @@ import java.time.LocalDateTime;
 public class UserRegister {
     private String email;
     private String password;
+    private Role role;
     private LoginType type;
     private String nickname;
     private Gender gender;
     private LocalDateTime birthday;
 
     @Builder
-    public UserRegister(String email, String password, LoginType type, String nickname,
+    public UserRegister(String email, String password, Role role, LoginType type, String nickname,
                         Gender gender, LocalDateTime birthday) {
         this.email = email;
         this.password = password;
@@ -28,12 +30,14 @@ public class UserRegister {
         this.nickname = nickname;
         this.gender = gender;
         this.birthday = birthday;
+        this.role = role;
     }
 
     public TbUser toEntity() {
         return TbUser.builder()
                 .email(this.email)
                 .password(this.password)
+                .role(this.role)
                 .type(this.type)
                 .nickname(this.nickname)
                 .gender(this.gender)
